eb72a88b53214b4b134ca068a5c36015f5d279a0
onrolll <conrolonrolll@gmail.com>
2018-06-08 20:24:40 +0300

    Insertion Sort

---
+++ InsertionSort/InsertionSort/InsertionSort.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace InsertionSort
+{
+    public static class InsertionSort
+    {
+        public static void Sort(int[] arr)
+		{
+			for (int i = 1; i < arr.Length; i++)
+			{
+				int key = arr[i];
+				int j = i - 1;
+
+				//  Move elements in the range [0; i - 1] to the right, if they are larger than the key            
+                while (j >= 0 && arr[j] > key)
+				{
+					arr[j + 1] = arr[j];
+					j--;
+				}
+                // Assign the value of the key to the appropriate position
+				arr[j + 1] = key;
+			}
+		}
+    }
+}
---
+++ InsertionSort/InsertionSort/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			var arr = new int[] { 12, 25, 64, 3, 2, 94, 66, 13, 15, 4, 8 };
+
+			InsertionSort.Sort(arr);
+          
+            Console.WriteLine(string.Join(' ', arr));
+		}
+    }
+}
