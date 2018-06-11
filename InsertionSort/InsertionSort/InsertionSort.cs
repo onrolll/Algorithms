b92659a7d88361718a505873597d24a60ac6baad
onrolll <conrolonrolll@gmail.com>
2018-06-11 15:50:32 +0300

    Indentation fix

--- InsertionSort/InsertionSort/InsertionSort.cs
+++ InsertionSort/InsertionSort/InsertionSort.cs
@@ -10,13 +10,13 @@ namespace InsertionSort
 				int key = arr[i];
 				int j = i - 1;
 
-				//  Move elements in the range [0; i - 1] to the right, if they are larger than the key            
-                while (j >= 0 && arr[j] > key)
+				//  Move elements in the range [0; i - 1] to the right, if they are larger than the key
+				while (j >= 0 && arr[j] > key)
 				{
 					arr[j + 1] = arr[j];
 					j--;
 				}
-                // Assign the value of the key to the appropriate position
+				// Assign the value of the key to the appropriate position
 				arr[j + 1] = key;
 			}
 		}
