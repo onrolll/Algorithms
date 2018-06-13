b2ec9896fdd569cf1c605c7ba2ca0de8a7d793ce
onrolll <conrolonrolll@gmail.com>
2018-06-13 17:50:57 +0300

    Heap Sort Implementation

---
+++ HeapSort/HeapSort/HeapSort.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace HeapSort
+{
+    public static class HeapSort
+    {
+		public static void Sort(int[] arr)
+		{
+			// Build a MaxHeap out of an array;
+			for (int i = arr.Length / 2 - 1; i >= 0; i--)
+				Heapify(arr, arr.Length, i);
+			
+			for (int i = arr.Length - 1; i >= 0; i--)
+			{
+				Swap(arr, 0, i);
+				Heapify(arr, i, 0);
+			}
+		}
+
+		private static void Heapify(int[] arr, int sizeOfMaxHeap, int i)
+		{
+			int largest = arr[i];
+			int childIndex = 2 * i + 1;
+
+			if (childIndex < sizeOfMaxHeap)
+			{
+				if (childIndex + 1 < sizeOfMaxHeap && arr[childIndex + 1] > arr[childIndex])
+					childIndex++;
+				if (arr[childIndex] > largest)
+					largest = arr[childIndex];
+			}
+			// if largest is still arr[i], this iteration is done;
+			if (largest == arr[i]) return;
+
+			Swap(arr, i, childIndex);
+			Heapify(arr, sizeOfMaxHeap, childIndex);
+		}
+
+		private static void Swap(int[] arr, int i, int j)
+		{
+			int temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+		}
+	}
+}
---
+++ HeapSort/HeapSort/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HeapSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int[] arr = new int[] { 11, 2, 44, 56, 789, 34, 3, 15, 28, 40, 42, 605 };
+
+			HeapSort.Sort(arr);
+
+			Console.WriteLine(string.Join(' ', arr));
+		}
+    }
+}
