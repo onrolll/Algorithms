bb70b298a0acd8d11496a8a0a19fc1007f57e506
onrolll <conrolonrolll@gmail.com>
2018-06-18 10:28:34 +0300

    Shell Sort Implementation

---
+++ ShellSort/ShellSort/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ShellSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int[] arr = new int[] { 13, 45, 5, 7, 2, 1234, 23, -2, 34, 0 };
+
+			ShellSort.Sort(arr);
+
+			Console.WriteLine(string.Join(' ', arr));
+		}
+    }
+}
---
+++ ShellSort/ShellSort/ShellSort.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace ShellSort
+{
+    public static class ShellSort
+    {
+		public static void Sort(int[] arr)
+		{
+			// Using left/rightIndex for convenience & readability uses more lines of code 
+			int rightIndex, leftIndex, initialRight, gap;
+
+			for ( gap = arr.Length / 2; gap > 0; gap /= 2)
+			{
+				for (rightIndex = gap; rightIndex < arr.Length; rightIndex++)
+				{
+					leftIndex = rightIndex - gap;
+
+					// initialRight will be placed at the last left position bigger than it;
+					initialRight = arr[rightIndex];
+
+					while (leftIndex >= 0 && arr[leftIndex] > initialRight)
+					{
+						// Shift bigger than initialRight elements to the right by gap positions
+						arr[rightIndex] = arr[leftIndex];
+
+						// Set new right/leftIndex;
+						rightIndex -= gap;
+						leftIndex = rightIndex - gap;
+					}
+					// Place initialRight at the correct position;
+					arr[rightIndex] = initialRight;
+				}
+			}
+		}
+	}
+}
